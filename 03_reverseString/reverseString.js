ea3beb2f7083df7994a86f503f8c6ac58f044d1a
¨lewisthagichu¨ <lewisthagichu254@gmail.com>
2023-05-02 17:22:23 +0300

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+// Function that reverses a string
+const reverseString = function(word) {
+    let strLen = word.length;
+    string = [...word];
+    newStr = [];
 
-};
+    for (let i = strLen-1; i >= 0; i--) {
+        newStr.push(string[i]);
+    };
+    return newStr.join('').toString();
+}
 
 // Do not edit below this line
 module.exports = reverseString;
