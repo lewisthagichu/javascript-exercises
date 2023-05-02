8fdcd87a430216979383681e1e415531fe0a4fe7
¨lewisthagichu¨ <lewisthagichu254@gmail.com>
2023-05-02 17:04:13 +0300

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = [];
+    if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+        result.push(string);
+    }
+    return result.join("").toString();
+    } else if (string === '') {
+        return '';
+    } else {
+        return "ERROR";
+    }
 };
-
+console.log(repeatString('', 10));
 // Do not edit below this line
 module.exports = repeatString;
