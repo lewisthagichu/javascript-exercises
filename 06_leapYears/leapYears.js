1c9966ee2ef4626d17a3d6dfe10366e0b69eb3ae
¨lewisthagichu¨ <lewisthagichu254@gmail.com>
2023-05-03 10:12:23 +0300

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+// Function to check whether a given year is a leap year
+const leapYears = function(year) {
+    if (year % 100 === 0 && year % 400 !== 0) {return false}
+    else if (year % 4 !== 0) {return false}
+    else {return true}
 };
 
 // Do not edit below this line
