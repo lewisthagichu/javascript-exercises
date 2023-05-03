d4c32e64900be1aac25a2a582018069112533e40
¨lewisthagichu¨ <lewisthagichu254@gmail.com>
2023-05-03 10:38:07 +0300

    Complete task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+//Functions to convert Fahrenheit to Celsius and vice versa
+
+const convertToCelsius = function(temp) {
+  let celcius = (temp - 32) * (5/9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp *(9/5)) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
-
+console.log(convertToFahrenheit(73.2))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
